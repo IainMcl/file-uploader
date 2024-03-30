ab4234749e73751b85e3d95d59527a4670ed80b0
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2024-03-30 21:26:59 +0000

    Add config to worker

--- worker/worker.py
+++ worker/worker.py
@@ -8,15 +8,32 @@ import os
 import logging
 import pika
 from pika.adapters.blocking_connection import BlockingChannel
+from .config import Config
 
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def conect(host: str = "rabbitmq", quque_name: str = "upload") -> BlockingChannel:
+def conect(
+        host: str = "rabbitmq",
+        quque_name: str = "upload",
+        connection_attempts: int = 3,
+        connection_delay: int = 10) -> BlockingChannel:
+    """
+    Connect to RabbitMQ and return a channel
+
+    When starting if the RabbitMQ is not ready, the worker will try to connect to it every 10
+    seconds 3 times.
+
+    :param host: str: RabbitMQ host
+    :param quque_name: str: Queue name
+    :param connection_attempts: int: Number of connection attempts
+    :param connection_delay: int: Delay between connection attempts
+    """
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=host, connection_attempts=3, retry_delay=10))
+        pika.ConnectionParameters(host=host,
+                                  connection_attempts=connection_attempts,
+                                  retry_delay=connection_delay))
     channel = connection.channel()
 
     channel.queue_declare(queue=quque_name)
@@ -25,15 +42,24 @@ def conect(host: str = "rabbitmq", quque_name: str = "upload") -> BlockingChanne
 
 
 def main():
-    channel = conect()
+    c = Config()
+    log_level = c.get('logs', 'log_level')
+    logging.basicConfig(level=log_level)
+    channel = conect(
+        host=c.get('rabbitmq', 'host'),
+        quque_name=c.get('rabbitmq', 'queue')
+    )
 
     def callback(ch, method, properties, body):
-        logging.info(f" [x] Received {body}")
+        logging.info("Received %s", body)
+        logging.debug("Method: %s", method)
+        logging.debug("Properties: %s", properties)
+        logging.debug("Channel: %s", ch)
 
     channel.basic_consume(
         queue='upload', on_message_callback=callback, auto_ack=True)
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    logger.info('Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 
