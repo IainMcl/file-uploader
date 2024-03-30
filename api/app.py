b72238a00fb5569ce2e9c6c5e8ec47fbe5113826
IainMcl <34077975+Darthmazgar@users.noreply.github.com>
2024-03-30 14:45:30 +0000

    update comments

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# CORS middleware to allow cross-origin requests (if needed)
+# CORS middleware to allow cross-origin requests 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins, you may want to restrict this in production
@@ -21,6 +21,7 @@ async def ping():
 async def upload_file(file: UploadFile = File(...)):
     try:
         contents = await file.read()
+        # TODO: Send this to the rabbit queue for processing by worker
         # Process the file contents here (e.g., save it to disk, upload to a database, etc.)
         return JSONResponse(status_code=200, content={"message": "File uploaded successfully"})
     except Exception as e:
